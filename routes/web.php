bb8a603208923880f551f33f6641ab14e3c72e5e
sinanqwe <sinan.yilmaz197@gmail.com>
2021-12-17 20:08:40 +0300

    Admin panel implemented

--- routes/web.php
+++ routes/web.php
@@ -9,9 +9,16 @@ Route::get('/', function () {
     return view('home.index');
 });
 
+
+
 Route::get('/home', [HomeController::class, 'index'])->name('home');
 Route::get('/aboutus', [HomeController::class, 'aboutus'])->name('home');
+
+
+
+Route::get('/admin', [\App\Http\Controllers\Admin\HomeController::class, 'index'])->name('adminhome');
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
+
